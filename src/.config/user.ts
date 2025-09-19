2e3df472c6c6fcd6c92f39e24bb384cb2b56b74d
MoonWX <wangxiao3027@vip.qq.com>
2025-09-20 02:15:00 +0800

    修改滚动条，修改关于，新增helloworld页面

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,7 +1,81 @@
 import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
-  // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: {
+    title: '代码和远方',
+    subtitle: 'MoonWX',
+    author: 'MoonWX',
+    description: 'Code and Alive',
+    website: 'https://blog.moonwx.net/',
+    pageSize: 5,
+    socialLinks: [
+      {
+        name: 'home',
+        href: 'https://moonwx.net',
+      },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+    categoryMap: [{ name: '', path: '' }],
+    footer: [
+      '© 2022 - %year <a target="_blank" href="%website">%author</a>',
+      'Theme <a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">Typography</a> by <a target="_blank" href="https://moeyua.com">Moeyua</a>',
+      'Proudly published with <a target="_blank" href="https://astro.build/">Astro</a>',
+    ],
+  },
+  appearance: {
+    theme: 'system',
+    locale: 'zh-cn',
+    colorsLight: {
+      primary: '#2e405b',
+      background: '#ffffff',
+    },
+    colorsDark: {
+      primary: '#FFFFFF',
+      background: '#232222',
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+  seo: {
+    twitter: '',
+    meta: [],
+    link: [],
+  },
+  rss: {
+    fullText: true,
+  },
+  comment: {
+    // disqus: { shortname: "typography-astro" },
+  },
+  analytics: {
+    googleAnalyticsId: '',
+    umamiAnalyticsId: '',
+  },
+  latex: {
+    katex: false,
+  },
 }
